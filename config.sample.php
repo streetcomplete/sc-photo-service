2240fef598d10537bc1d2888d26e045d1ddbcd83
exploide <me@exploide.net>
2018-08-22 21:14:29 +0200

    save file (if valid photo) into temporary directory with random filename
    added sample config file and gitignore

---
+++ config.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+    $PHOTOS_TMP_DIR = '../photos_tmp';
+    $PHOTOS_SRV_DIR = '../photos_srv';
+
+    $ALLOWED_FILE_TYPES = array(
+        'image/jpeg' => '.jpg'
+    );
+
+?>
--- upload.php
+++ upload.php
@@ -1,5 +1,6 @@
 <?php
 
+    require_once 'config.php';
     require_once 'helper.php';
 
     header('Content-Type: application/json');
@@ -11,5 +12,12 @@
     $photo = file_get_contents("php://input");
     $finfo = new finfo(FILEINFO_MIME_TYPE);
     $file_type = $finfo->buffer($photo);
+
+    if(!array_key_exists($file_type, $ALLOWED_FILE_TYPES)) {
+        return_error(415, 'File type not allowed');
+    }
+
+    $file_name = bin2hex(random_bytes(16)) . $ALLOWED_FILE_TYPES[$file_type];
+    file_put_contents($PHOTOS_TMP_DIR . '/' . $file_name, $photo);
 
 ?>
