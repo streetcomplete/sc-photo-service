470283cc7061c7cd3efb2ed266612e6bffd52c85
Richard Kuhnt <r15ch13+git@gmail.com>
2018-09-15 12:43:27 +0200

    Revert to use unlink

--- cleanup.php
+++ cleanup.php
@@ -10,18 +10,10 @@ require_once 'osm_photo_note.php';
 
 $db_helper = new DBHelper();
 
-function deleteFile($file_path)
-{
-    echo "Deleting $file_path\n";
-    if (file_exists($file_path)) {
-        unlink($file_path);
-    }
-}
-
 $old_inactive_photos = $db_helper->getAndDeleteOldInactivePhotos();
 foreach ($old_inactive_photos as $photo) {
     $file_path = Config::PHOTOS_TMP_DIR . DIRECTORY_SEPARATOR . $photo['file_id'] . $photo['file_ext'];
-    deleteFile($file_path);
+    unlink($file_path);
 }
 
 $active_photos = $db_helper->getActivePhotos();
@@ -31,20 +23,20 @@ foreach ($active_photos as $photo) {
 
     if ($osm_note->http_code === 404 or $osm_note->http_code === 410) {
         $db_helper->deletePhoto($photo['file_id']);
-        deleteFile($file_path);
+        unlink($file_path);
     }
 
     if ($osm_note->http_code === 200) {
         if (!in_array($photo['file_id'], $osm_note->photo_ids)) {
             $db_helper->deletePhoto($photo['file_id']);
-            deleteFile($file_path);
+            unlink($file_path);
         }
 
         if ($osm_note->status === 'closed'
             && strtotime($osm_note->closed_at . ' +' . (int)Config::MAX_LIFETIME_AFTER_NOTE_CLOSED_DAYS . ' days') < strtotime('now')
         ) {
             $db_helper->deletePhoto($photo['file_id']);
-            deleteFile($file_path);
+            unlink($file_path);
         }
     }
 }
